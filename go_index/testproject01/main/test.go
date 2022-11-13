fdc61024a1b3159caf967cfc28a5becac922333c
hhx <>
2022-11-13 18:15:29 +0800

    str判断

--- go_index/testproject01/main/test.go
+++ go_index/testproject01/main/test.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("hello GoLang!")
+
+	fmt.Println(len("你好"))                    //输出6，计算的是字节
+	fmt.Println(utf8.RuneCountInString("你好")) //输出2
 }
