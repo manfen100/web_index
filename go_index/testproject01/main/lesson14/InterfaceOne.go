a7f3cceb6e1a0aea30471daf49436d75e7a0933e
hhx <>
2022-12-01 14:32:26 +0800

    go for for

--- go_index/testproject01/main/lesson14/InterfaceOne.go
+++ go_index/testproject01/main/lesson14/InterfaceOne.go
@@ -10,4 +10,12 @@ func main() {
 	SHowType(str)
 	num := 3.14
 	SHowType(num)
+
+	x := "hello"
+	for _, x := range x {
+		x := x + 'A' - 'a'
+		fmt.Printf("%c", x) // "HELLO" (one letter per iteration)
+	}
 }
+
+var a string
