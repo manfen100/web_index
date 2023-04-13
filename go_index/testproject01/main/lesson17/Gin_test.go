9fd80a7bc1649425923368517267d0556fdf7ac8
hhx <>
2023-04-13 18:48:10 +0800

    go for Mutex

--- go_index/testproject01/main/lesson17/Gin_test.go
+++ go_index/testproject01/main/lesson17/Gin_test.go
@@ -1,5 +1,20 @@
 package lesson17
 
-func main() {
+import "sync"
 
+type singleton struct {
+}
+
+var ins *singleton
+var mu sync.Mutex
+
+func GetIns() *singleton {
+	if ins == nil {
+		mu.Lock()
+		if ins == nil {
+			ins = &singleton{}
+		}
+		mu.Unlock()
+	}
+	return ins
 }
